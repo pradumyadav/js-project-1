ae9fdb1488013bd05a02dd82244fcbd756921468
pradumyadav <122624556+pradumyadav@users.noreply.github.com>
2023-06-14 12:48:20 +0530

    uploded 2nd time

---
+++ index.js
@@ -0,0 +1,155 @@
+
+
+
+let HOURS=document.querySelector('.X')
+let MIN=document.querySelector('.Y')
+let SEC=document.querySelector('.Z')
+let output2=document.querySelector('.box1');
+function callhear(){
+   let time=new Date();
+   let hours=time.getHours();
+   let mins=time.getMinutes();
+   let sec=time.getSeconds();
+   if(hours>=1 && hours<=7){
+      output2.innerHTML='GOOD NIGHT EVERY ONE'
+   }
+   else if(hours>=7&& hours<=12){
+      output2.innerHTML="GOOD MORNING EVERY ONE"
+
+   }
+   else if(hours>=12 && hours<=18){
+      output2.innerHTML="GOOD AFTER NOON EVERY ONE"
+   }
+   else if(hours>=18 && hours<=24){
+      output2.innerHTML="GOOD EVENING EVERY ONES"
+      
+   }
+    if(hours>12){
+       hours=hours-12;
+    }
+HOURS.innerText=hours
+MIN.innerText=mins
+SEC.innerText=sec
+    
+   }
+setInterval(()=>{
+   callhear()
+},1000);
+ 
+let img =document.getElementById('box5')
+let output1=document.querySelector('.child1');
+// let image=document.querySelector('img');
+
+let morning_time=document.getElementById('s1');
+let lunch_time=document.getElementById('s2')
+let nap_time=document.getElementById('s3')
+let night_time=document.getElementById('s4')
+
+// let morning=document.getElementById('s1');
+// let lunch=document.getElementById('s2')
+// let nap=document.getElementById('s3')
+// let night=document.getElementById('s4')
+let show_options1=document.getElementById('change1')
+let show_options2=document.getElementById('change2')
+let show_options3=document.getElementById('change3')
+let show_options4=document.getElementById('change4')
+
+function clickhear(){
+   let date=new Date()
+   let hrs=date.getHours();
+   // if (hrs>12){hrs=hrs-12}
+   // console.log(hrs);
+   // console.log(morning_time.value)
+   if (hrs === parseInt( morning_time.value)) {
+      console.log(morning_time.value);
+      output1.innerHTML = "GOOD MORNING!! WAKE UP !!";
+  
+      img.style.backgroundImage = "url('./mrng.png')";
+
+    } else if (hrs === parseInt(lunch_time.value)) {
+      console.log(lunch_time.value);
+      output1.innerHTML = "GOOD AFTERNOON !! TAKE SOME SLEEP ";
+  
+      img.style.backgroundImage = "url('./luch.png')";
+
+    } else if (hrs === parseInt(nap_time.value)) {
+      console.log(nap_time.value);
+      output1.innerHTML = "GOOD EVENING !! ";
+  
+      img.style.backgroundImage = "url('./goodevening.png')";
+
+    } else if (hrs === parseInt(night_time.value)) {
+      console.log(night_time.value);
+      output1.innerHTML = "GOOD NIGHT !!";
+  
+      img.style.backgroundImage = "url('./GOODNIGHT.png')";
+    }
+    let morningtext = morning_time.options[morning_time.selectedIndex].textContent;
+  let morning = document.getElementById("change1");
+  morning.textContent = morningtext;
+
+  let morningtext1 = lunch_time.options[lunch_time.selectedIndex].textContent;
+  let morning1 = document.getElementById("change2");
+  morning1.textContent = morningtext1;
+
+  let morningtext2 = nap_time.options[nap_time.selectedIndex].textContent;
+  let morning2 = document.getElementById("change3");
+  morning2.textContent = morningtext2;
+
+  let morningtext3 = night_time.options[night_time.selectedIndex].textContent;
+  let morning3 = document.getElementById("change4");
+  morning3.textContent = morningtext3;
+
+};
+
+
+
+// if(parseInt(morning_time.value) === hrs){
+//    output1.innerHTML='GOOD MORNING!!WAKE UP!!'
+//    image.src="./Component 30 - 1.svg"
+// }
+// else if(parseInt(lunch_time.value === hrs)){
+//    output1.innerHTML="Let's go for lunch"
+//    image.src="./Component 31 - 1.svg"
+// }
+// else if(parseInt(nap_time.value) === hrs){
+//    output1.innerHTML="Go for sleep"
+//    image.src="./lunch_time.svg"
+// }
+// else if(parseInt(night_time.value) === hrs){
+//    output1.innerHTML="Good Night"
+//    image.src="./Component 32 - 1.svg"
+// }
+// else{
+//    console.log('sorry')
+// }
+
+// let selectindex1=morning.selectedIndex ;
+// let targetindex1=morning.options[selectindex1]
+// show_options1.innerHTML=targetindex1.innerHTML
+
+// let selectindex2=lunch.selectedIndex ;
+// let targetindex2=lunch.options[selectindex2]
+// show_options2.innerHTML=targetindex2.innerHTML
+
+// let selectindex3=nap.selectedIndex ;
+// let targetindex3=nap.options[selectindex3]
+// show_options3.innerHTML=targetindex3.innerHTML
+
+// let selectindex4=night.selectedIndex ;
+// let targetindex4=night.options[selectindex4]
+// show_options4.innerHTML=targetindex4.innerHTML
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
